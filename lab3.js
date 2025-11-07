a88fd574c8dafd8c2856ac54027a6339c762507d
VietHoang <hoangbui281206@gmail.com>
2025-11-07 15:07:18 +0700

    Cập nhật file lab3.js

---
+++ lab3.js
@@ -0,0 +1,9 @@
+const multiply = (a ,b) => a*b;
+
+const isPositive = (number) => number >= 0;
+
+const getRandomNumber = () => Math.random();
+
+document.addEventListener("click",  () => {
+    console.log("Clicked!");
+  });
